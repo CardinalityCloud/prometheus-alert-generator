51ce7159c2cfda943a1ca36eafeb31e75a6f0aa3
Jack Neely <jjneely@linuxczar.net>
2025-10-04 22:57:06 -0400

    Handle 404s better.
    We need to set the ALB to return index.html on error with a 200 code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import PrometheusRuleGenerator from './prometheus-rule-generator.tsx';
 import { Faq } from './Faq.tsx';
+import { NotFound } from './NotFound.tsx';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<PrometheusRuleGenerator />} />
           <Route path="/faq" element={<Faq />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </MantineProvider>
